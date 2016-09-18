4733116464eaa951cc331ca824314eda376d65d0
Ian Agpawa <agpawaji@gmail.com>
2016-09-18 18:29:35 -0400

    get shortening

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-var mongodb = require('mongodb');
+var mongo = require('mongodb').MongoClient;
 var shortid = require('shortid');
 var validUrl = require('valid-url');
 
-router.get('/new/:url(*)', function (req, res, next) {
-  res.send(req.params.url);
-});
+var mLab = "mongodb://localhost:27017/url-shortener-microservice";
+
+
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/:url(*)', function (req, res, next) {
+  mongo.connect(mLab, function (err, db) {
+    if (err) throw err
+    console.log("Connected")
+
+    var collection = db.collection('links');
+    var params = req.params.url;
+
+    var newLink = function (db, callback) {
+
+      if (validUrl.isUri(params)){
+        var shortened = shortid.generate();
+        var insertObj = { url: params, short: shortened}
+        collection.insert(insertObj)
+        res.json({
+          original: params,
+          shorten: "localhost:3000/" + shortened
+        })
+      } else {
+        res.json({
+          error: "Invalid URL"
+        })
+      }
+
+      /*
+      var insertLink = { url: params, short: "short" };
+      collection.insert([insertLink]);
+      */
+
+
+    };
+
+    newLink(db, function () {
+      db.close();
+    });
+
+
+  });
+
+
+});
+
 
 
 
